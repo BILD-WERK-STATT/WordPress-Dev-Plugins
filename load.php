6435cad5282756a7fe0dfe032b42b3961d4065a1
araeubig <129953354+araeubig@users.noreply.github.com>
2023-04-30 13:10:24 +0200

    Create load.php

---
+++ load.php
@@ -0,0 +1,5 @@
+<?php // mu-plugins/load.php
+require WPMU_PLUGIN_DIR.'/loco-translate/loco.php';
+require WPMU_PLUGIN_DIR.'/query-monitor/query-monitor.php';
+require WPMU_PLUGIN_DIR.'/tdd-debug-bar-post-meta/debug-bar-post-meta.php';
+require WPMU_PLUGIN_DIR.'/transients-manager/transients-manager.php';
